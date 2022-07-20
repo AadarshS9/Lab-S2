799e78b38089bf95c2402ad92f59ef345c4805ae
Aadarsh Suresh <94780937+AadarshS9@users.noreply.github.com>
2022-07-20 16:09:41 +0530

    Update Mergesort.c

--- Week 10: Q,M Sorting/Mergesort.c
+++ Week 10: Q,M Sorting/Mergesort.c
@@ -4,7 +4,6 @@
 #include <time.h>
 int array[10000];
 void mergesort (int [], int, int);
-void swap(int*, int*);
 float timedifference_msec(struct timeval t0, struct timeval t1)
 {
     return (t1.tv_sec - t0.tv_sec) * 1000.0f + (t1.tv_usec - t0.tv_usec) / 1000.0f;
